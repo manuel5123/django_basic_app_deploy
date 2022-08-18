d7a60ad50d533791dc4395a9d35ecae82d491f43
manuel5123 <manuel123@posteo.de>
2022-08-19 00:31:49 +0200

    STATIC_ROOT added in settings.py

--- deploy_app/deploy_app/settings.py
+++ deploy_app/deploy_app/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
